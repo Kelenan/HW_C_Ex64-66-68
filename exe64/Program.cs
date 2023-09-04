09da0c5ff2abc2ff00c792a3016276cb0c3bf074
«Elena <leseee4ka@bk.ru>
2023-09-04 21:34:53 +0300

    exe

---
+++ exe64/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine(num);
+
+
+string a1 = GetNatur (num, 1);
+
+Console.WriteLine("-----");
+Console.WriteLine(a1);
+
+
+
+string GetNatur (int startNum, int inNum)
+{
+    if (startNum == 1) return inNum.ToString();
+    return startNum + " " + GetNatur(startNum-1, inNum);
+}
+
---
+++ exe66/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+Console.Clear();
+
+Console.Write("Введите число M: ");
+int numM = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите число N: ");
+int numN = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine($"{numM} / {numN}");
+
+ if (numM > numN)
+{
+  int tmp = numM;
+  numM = numN;
+  numN = tmp;  
+} 
+
+int sum1 = 0;
+sum1 = GetNatur(numM, numN);
+
+Console.WriteLine("-----");
+Console.WriteLine(sum1);
+
+
+int GetNatur (int numM, int numN)
+{
+    if (numM == 0 && numN == 0) return 0;
+    if (numM == 1 && numN == 1) return 1;
+    if (numM > numN) return sum1;
+    return numM + GetNatur(numM+1, numN);
+}
+
